14289420010a624fc329ec04709616304dad6812
ljknight <ljknight@gmail.com>
2015-08-27 16:47:17 -0700

    Add audio, add pridefy, add hipster passOut, update burrito to gif

--- src/hipsterDancer.js
+++ src/hipsterDancer.js
@@ -1,8 +1,7 @@
 
 var makeHipsterDancer = function(top, left, timeBetweenSteps){
   makeDancer.apply(this, arguments);
-  // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
-  // so we must keep a copy of the old version of this function
+  this.callPassOut();
 };
 
 makeHipsterDancer.prototype = Object.create(makeDancer.prototype);
@@ -10,17 +9,19 @@ makeHipsterDancer.prototype.constructor = makeHipsterDancer;
 
 
 makeHipsterDancer.prototype.step = function(){
-  // call the old version of step at the beginning of any call to this new version of step
   makeDancer.prototype.step.call(this);
-  //  setTimeout(this.step, timeBetweenSteps);
-
-  // toggle() is a jQuery method to show/hide the <span> tag.
-  // See http://api.jquery.com/category/effects/ for this and
-  // other effects you can use on a jQuery-wrapped html tag.
   this.$node.addClass('hipster');
-  this.$node.toggle();
 };
 
 makeHipsterDancer.prototype.show = function(){
   this.$node.css('display', 'none');
+};
+
+makeHipsterDancer.prototype.passOut = function(){
+  this.$node.css('transform', 'rotate(90deg)');
+};
+
+makeHipsterDancer.prototype.callPassOut = function(){
+  var context = this;
+   setTimeout(function() {context.passOut()}, 1500);
 };
--- src/init.js
+++ src/init.js
@@ -46,9 +46,42 @@ $(".cancanButton").on("click", function(event){
     }
   });
 
+$(".prideButton").on("click", function(event){
+  $(".prideBars").toggle();
+});
+
+$(".Hipster").on("click", function(event){
+    var hipsterMakerFunction = window["makeHipsterDancer"];
+    var hipster = new hipsterMakerFunction(
+      $("body").height() * Math.random(),
+      $("body").width() * Math.random(),
+      Math.random() * 1000
+    );
+    var hipster1 = new hipsterMakerFunction(
+      $("body").height() * Math.random(),
+      $("body").width() * Math.random(),
+      Math.random() * 1000
+    );
+    $('body').append(hipster.$node);
+    $('body').append(hipster1.$node);
+    window.dancers.push(hipster, hipster1);
+  $(".overlay").toggle();
+  document.getElementById('tswift').paused ? document.getElementById('tswift').play() : document.getElementById('tswift').pause();
+  });
+
+$("body").on("mouseover", ".hipster", function(){
+    hipsterMove($(this), '110%', 30000);
+  });
+
 $("body").on("mouseover", ".dog", function(){
     doBounce($(this), 3, '100px', 300);
   });
+
+function hipsterMove(element, speed) {
+  var plusOrMinus = Math.random() < 0.5 ? -1 : 1;
+  var distance = plusOrMinus * 350;
+  element.animate({left: '-='+distance.toString()}, speed);        
+}
 
 function doBounce(element, times, distance, speed) {
     for(var i = 0; i < times; i++) {
